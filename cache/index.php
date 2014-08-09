43e5e1c237720a6e9782a9863b7fc65938df00b7
Guilherme <guilherme@guilherme-VirtualBox.(none)>
2014-08-09 18:23:59 -0300

    Cache Folder

---
+++ cache/index.php
@@ -0,0 +1,15 @@
+ <?php
+ header("Cache-Control: max-age=10");
+ ?>
+<!doctype html>
+<html>
+	<head>
+		<title>Max-Age</title>
+	</head>
+	<body>
+		<div>
+		Index
+		</div>
+		<img src="ilha.jpg" width="40%">
+	</body>
+</html>
