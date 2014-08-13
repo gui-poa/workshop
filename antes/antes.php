644880ebd16e322e2b403cf3829be3d8b3b0250a
Guilherme <guilherme@guilherme-VirtualBox.(none)>
2014-08-12 22:36:01 -0300

    Primeira parte do Antes

---
+++ antes/antes.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+	<head>
+	     <title>Antes - Workshop WEB Performance</title>
+	     <srcipt src="http://gui-poa.github.io/talks/mobileperf/waiting.js">
+	     <link rel="stylesheet" type="text/css" href="http://gui-poa.github.io/talks/mobileperf/waiting.css">
+	</head>
+
+	<body>
+		<div id="teste">
+			<span id="logo"></span>
+		</div>
+
+		<div id="teste2">
+			<p>Web Performance - Antes</p>
+			<?php
+				include "../flush/db.php"
+			?>
+		</div>
+		<div>
+			<img src="ilha.jpg" width="60%">
+		</div>
+	</body>
+</html>
---
+++ antes/index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+	     <title>Redir - Workshop WEB Performance</title>
+	     <script>
+	     	window.location = "antes.php"
+     	</script>
+	</head>
+</html>
