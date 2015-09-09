f5aaeb847aaff1904f4f36d6791bc43bffe07702
Guilherme <guilhemezz@gmal.com>
2015-09-08 23:28:27 -0300

    New file to test preconnect link

--- flush/db.php
+++ flush/db.php
@@ -1,11 +1,10 @@
 <?php
 
-for ($i=0;$i<40000000;$i++)
+for ($i=0;$i<80000000;$i++)
 {
 	//Manipulacao dos resultados do Banco de Dados
 	if ($i%5000000 == 0)
 		$acao = "OK";
 }
 
-print "DB query finished";
 ?>
